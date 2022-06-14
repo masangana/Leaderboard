b4ddd7a7b653b31c5e3cd679de4b03b269ade4f7
masangana <alexmasangana@gmail.com>
2022-06-15 00:38:00 +0300

    add plate html

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ import './style.scss';
 
 const title = document.createElement('h2');
 title.textContent = 'Pas facile les framework';
-const page = document.querySelector('section');
-page.append(title)
+//const page = document.querySelector('section');
+//page.append(title)
 
-function component() {
-    const element = document.createElement('div');
- 
-   // Lodash, currently included via a script, is required for this line to work
-   // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- 
-    return element;
-  }
- 
-  document.body.appendChild(component());
