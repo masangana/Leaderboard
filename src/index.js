4d09dc88da1566c4aa3e3e286f7b6164ce714d6a
masangana <alexmasangana@gmail.com>
2022-06-15 01:16:01 +0300

    add html & css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import './style.scss';
 
 const title = document.createElement('h2');
 title.textContent = 'Pas facile les framework';
-//const page = document.querySelector('section');
-//page.append(title)
 
