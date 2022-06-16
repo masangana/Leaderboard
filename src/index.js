9957cc0f88f18c71390eb936d610f16fd72da525
masangana <alexmasangana@gmail.com>
2022-06-16 07:06:10 +0300

    Add Api
    --Create module
    --Add localstorage
    --Add board & vue files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,52 @@
 import 'bootstrap';
 import './style.scss';
+import LeaderBoard from './modules/board.js';
+import listvue from "./modules/vue.js";
 
-const title = document.createElement('h2');
-title.textContent = 'Pas facile les framework';
+const reload = document.getElementById('reload');
+const submit = document.getElementById('submit')
+const user = document.getElementById('user')
+const score = document.getElementById('score')
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/au9ydNy6JgFBAIbZfr3Q/scores/';
+
+const collection = new LeaderBoard(url);
+
+
+
+
+const newliste = new listvue();
+const arr = JSON.parse(localStorage.getItem('dataGame'));
+
+
+collection.getCollection()
+newliste.showlist(arr);
+
+//reload button
+reload.addEventListener('click', () => {
+    collection.getCollection();
+    if (localStorage.getItem('dataGame')) {
+      const arr = JSON.parse(localStorage.getItem('dataGame'));
+      newliste.showlist(arr);
+    }
+  });
+
+
+//submit button
+submit.addEventListener('click', () => {
+    if (!user.value || !score.value) {
+        user.setCustomValidity('Please check the user name or the score');
+    } else if(user.value && score.value) {
+        collection.addToCollection(user.value.trim(), score.value.trim());
+        user.value = '';
+        score.value = '';
+    }
+  });
+
+// when the page loadding
+window.onload = () => {
+    if (localStorage.getItem('dataGame')) {
+      const arr = JSON.parse(localStorage.getItem('dataGame'));
+      newliste.showlist(arr);
+    }
+  };
---
+++ src/modules/board.js
@@ -0,0 +1,34 @@
+import { data } from "autoprefixer";
+
+export default class LeaderBoard {
+    constructor(url){
+        this.url = url;
+    }
+
+    getCollection = async() =>{
+        await fetch(this.url)
+        .then((Response) => Response.json())
+        .then((data) => {
+            localStorage.setItem('dataGame', JSON.stringify(data));  
+        })
+    }
+
+    mydata = async()=>{
+        const response = await fetch(this.url);
+        const jsonResult = await response.json();
+        return jsonResult.result;
+    }
+
+    addToCollection = async (user,score) => {
+        await fetch(this.url, {
+            method: 'POST',
+            body: JSON.stringify({ 
+                "user": user,
+                "score": score
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        }).then((response) => response.json())
+    }
+}
