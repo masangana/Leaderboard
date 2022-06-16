1d7fad15886c2be67f6c4e250ad4f9cc0ded6ac0
masangana <alexmasangana@gmail.com>
2022-06-16 12:10:32 +0300

    fix linters error

--- src/modules/board.js
+++ src/modules/board.js
@@ -1,34 +1,32 @@
-import { data } from "autoprefixer";
-
 export default class LeaderBoard {
-    constructor(url){
-        this.url = url;
-    }
+  constructor(url) {
+    this.url = url;
+  }
 
-    getCollection = async() =>{
-        await fetch(this.url)
+    getCollection = async () => {
+      await fetch(this.url)
         .then((Response) => Response.json())
         .then((data) => {
-            localStorage.setItem('dataGame', JSON.stringify(data));  
-        })
+          localStorage.setItem('dataGame', JSON.stringify(data));
+        });
     }
 
-    mydata = async()=>{
-        const response = await fetch(this.url);
-        const jsonResult = await response.json();
-        return jsonResult.result;
+    mydata = async () => {
+      const response = await fetch(this.url);
+      const jsonResult = await response.json();
+      return jsonResult.result;
     }
 
-    addToCollection = async (user,score) => {
-        await fetch(this.url, {
-            method: 'POST',
-            body: JSON.stringify({ 
-                "user": user,
-                "score": score
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        }).then((response) => response.json())
+    addToCollection = async (user, score) => {
+      await fetch(this.url, {
+        method: 'POST',
+        body: JSON.stringify({
+          user,
+          score,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }).then((response) => response.json());
     }
 }
